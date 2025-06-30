36d8764e669158651738eb4c348a322a5b31e38a
Naman Anand <46650369+void-memories@users.noreply.github.com>
2025-06-30 13:30:34 +0530

    [ANDROID] Impl NimbleNet Internal (#34)
    * init base
    Signed-off-by: Naman Anand <naman.anand@nimbleedgehq.ai>
    
    * add headers
    Signed-off-by: Naman Anand <naman.anand@nimbleedgehq.ai>
    
    * add headers to .proto
    Signed-off-by: Naman Anand <naman.anand@nimbleedgehq.ai>
    
    * add headers to .yml
    Signed-off-by: Naman Anand <naman.anand@nimbleedgehq.ai>
    
    * add androidTests
    Signed-off-by: Naman Anand <naman.anand@nimbleedgehq.ai>
    
    * add headers
    Signed-off-by: Naman Anand <naman.anand@nimbleedgehq.ai>
    
    * revert nnc
    Signed-off-by: Naman Anand <naman.anand@nimbleedgehq.ai>
    
    * add tests
    Signed-off-by: Naman Anand <naman.anand@nimbleedgehq.ai>
    
    * add gemini handler
    Signed-off-by: Naman Anand <naman.anand@nimbleedgehq.ai>
    
    * add headers
    Signed-off-by: Naman Anand <naman.anand@nimbleedgehq.ai>
    
    * add internal apis
    Signed-off-by: Naman Anand <naman.anand@nimbleedgehq.ai>
    
    * add headers
    Signed-off-by: Naman Anand <naman.anand@nimbleedgehq.ai>
    
    * rename
    Signed-off-by: Naman Anand <naman.anand@nimbleedgehq.ai>
    
    * add package
    Signed-off-by: Naman Anand <naman.anand@nimbleedgehq.ai>

---
+++ sdks/android/nimblenet_ktx/src/internal/kotlin/dev/deliteai/NimbleNetInternal.kt
@@ -0,0 +1,56 @@
+/*
+ * SPDX-FileCopyrightText: (C) 2025 DeliteAI Authors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package dev.deliteai
+
+import dev.deliteai.controllers.InternalTaskController
+import dev.deliteai.datamodels.NimbleNetResult
+import dev.deliteai.impl.DependencyContainer
+import android.app.Application
+
+object NimbleNetInternal {
+    @Volatile private lateinit var controller: InternalTaskController
+
+    private fun getController(application: Application): InternalTaskController {
+        return DependencyContainer.getInstance(null, null).getInternalTaskController().also {
+            controller = it
+        }
+    }
+
+    fun initialize(application: Application) {
+        getController(application = application)
+    }
+
+    fun resetAppState() {
+        controller.resetAppState()
+    }
+
+    fun reset() {
+        controller.reset()
+    }
+
+    fun deleteDatabase() {
+        controller.deleteDatabase()
+    }
+
+    fun reloadModelWithEpConfig(modelName: String, epConfig: String): NimbleNetResult<Unit> {
+        return controller.reloadModelWithEpConfig(modelName, epConfig)
+    }
+
+    fun loadModelFromFile(
+        modelFilePath: String,
+        inferenceConfigFilePath: String,
+        modelId: String,
+        epConfigJson: String,
+    ): NimbleNetResult<Unit> {
+        return controller.loadModelFromFile(
+            modelFilePath,
+            inferenceConfigFilePath,
+            modelId,
+            epConfigJson,
+        )
+    }
+}
